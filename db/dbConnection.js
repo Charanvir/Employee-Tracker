e8fb4931e7570614d2334b9c5ff276100ab526a7
Charanvir <charanvir123@gmail.com>
2022-04-29 01:11:35 -0400

    comments added explaining code

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql2');
+// To protect SQL user and password credentials, a separate file has been created and is being referenced in the connection call below
+// The credentials file is included in the .gitignore file so it is not exposed on GitHub
 const credentials = require("./credentials")
 
 // Connect to database
