8d4607c7e94789d72880c126fbbeba86aa44fed7
Charanvir <charanvir123@gmail.com>
2022-05-02 04:31:32 -0400

    seeds.sql updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,14 +9,14 @@ VALUES
 
 INSERT INTO roles (title, salary, department_id)
 VALUES 
-    ('Backend Team Lead', 130000, 1),
-    ('Backend Engineer', 110000, 1),
-    ('Frontend Team Lead', 90000, 2),
-    ('Frontend Engineer', 90000, 2),
-    ('Full Stack Team Lead', 150000, 3),
-    ('Full Stack Engineer', 120000, 3),
-    ('Quality Assurance Team Lead', 120000, 4),
-    ('Quality Assurance', 120000, 4);
+    ('Backend Team Lead', 150000, 1),
+    ('Backend Engineer', 120000, 1),
+    ('Frontend Team Lead', 150000, 2),
+    ('Frontend Engineer', 110000, 2),
+    ('Full Stack Team Lead', 175000, 3),
+    ('Full Stack Engineer', 130000, 3),
+    ('Quality Assurance Team Lead', 100000, 4),
+    ('Quality Assurance', 80000, 4);
 
 INSERT INTO employees (first_name, last_name, roles_id, manager_id)
 VALUES
